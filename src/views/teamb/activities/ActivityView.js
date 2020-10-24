1db5367596b43baa959692636d85500f18807118
juliethhurtado <juliethhs@unicauca.edu.co>
2020-10-23 19:00:12 -0500

    indentation and dates

--- src/views/teamb/activities/ActivityView.js
+++ src/views/teamb/activities/ActivityView.js
@@ -11,65 +11,13 @@ import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import { Link as RouterLink } from 'react-router-dom';
 import service from 'src/views/teamb/activities/service';
-
-
-import {
-
-  Grid,
-  Select,
-  TextField,
-  MenuItem
-} from '@material-ui/core';
+import { Grid, Select, TextField, MenuItem, InputLabel } from '@material-ui/core';
 import { render } from 'nprogress';
-
 const ObtPeriodo = new service();
-const states = [
-
-  {
-    value: 'advert',
-    label: 'Seleccione una opción'
-  },
-  {
-    value: 'T1',
-    label: 'Curso, dirección/revisión de proyectos'
-  },
-  {
-    value: 'T2',
-    label: 'Ponencias en congresos, simposios y/o jornadas'
-  },
-  {
-    value: 'T3',
-    label: 'Publicaciones'
-  },
-  {
-    value: 'T4',
-    label: 'Exposición de resultados parciales de investigación'
-  },
-  {
-    value: 'T5',
-    label: 'Estancias de investigación en otras instituciones:'
-  },
-  {
-    value: 'T6',
-    label: 'participación en proyectos de investigación'
-  }
-];
-
 const styles = (theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
+  root: { margin: 0, padding: theme.spacing(2) },
+  closeButton: { position: 'absolute', right: theme.spacing(1), top: theme.spacing(1), color: theme.palette.grey[500] },
 });
-
-
-
 const DialogTitle = withStyles(styles)((props) => {
   const { children, classes, onClose, ...other } = props;
   return (
@@ -83,51 +31,22 @@ const DialogTitle = withStyles(styles)((props) => {
     </MuiDialogTitle>
   );
 });
-
 const DialogContent = withStyles((theme) => ({
-  root: {
-    padding: theme.spacing(2),
-  },
+  root: { padding: theme.spacing(2) },
 }))(MuiDialogContent);
-
 const DialogActions = withStyles((theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
+  root: { margin: 0, padding: theme.spacing(1) },
 }))(MuiDialogActions);
-
-
 
 const ActivityView = ({ className, ...rest }) => {
   const refPeriodo = React.createRef();
   const [open, setOpen] = React.useState(false);
   const [activity, setActivity] = React.useState("");
-
-  const changeActivityType = (e) => {
-    setActivity(e)
+  const changeActivityType = (e) => { 
+    setActivity(e) 
   }
-
   const [values, setValues] = useState({
-
   });
-  /*const TypesForm = () => {
-    const selected = document.getElementById("tipo").value;
-    
-    switch(selected){
-      case 'T1':
-        render(){
-          return <div>Esto</div>;
-          
-        }
-
-      break;
-    default:
-      break;
-    }
-     
-
-  }*/
   const handleClickOpen = () => {
     ObtPeriodo.ObtPeriodoService({ "periodo": refPeriodo }).then((result) => {
       alert("pais creado");
@@ -146,36 +65,20 @@ const ActivityView = ({ className, ...rest }) => {
     });
   };
 
-
   return (
     <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        CREAR ACTIVIDAD
-      </Button>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}> CREAR ACTIVIDAD </Button>
       <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
         <br></br>
         <h1 style={{ display: 'flex', justifyContent: 'center' }}>Crear actividad</h1>
         <br></br>
-        <label ref={refPeriodo} style={{ display: 'flex', justifyContent: 'center' }} id="periodoAct" title="periodo"> Periodo 2019-2020</label>
+        <InputLabel ref={refPeriodo} style={{ display: 'flex', justifyContent: 'center' }} id="periodoAct" title="periodo"> Periodo 2019-2020</InputLabel>
         <br></br>
         <DialogContent dividers>
-
-          <Grid
-            item
-
-            xs={12}
-          >
-            <label>Tipo de actividad:</label>
-            <Select
-              fullWidth
-              label="Tipo de actividad"
-              id="activity-type"
-              variant="outlined"
-              type="select"
-              defaultValue={""}
-              onChange={e => changeActivityType(e.target.value)  }
-
-            >
+          <Grid item xs={12} >
+            <InputLabel>Tipo de actividad:</InputLabel>
+            <Select fullWidth label="Tipo de actividad" id="activity-type" variant="outlined" type="select" defaultValue={""}
+              onChange={e => changeActivityType(e.target.value)}>
               <MenuItem value={'activityone'}>Curso, dirección/revisión de proyectos</MenuItem>
               <MenuItem value={'activitytwo'}>Ponencias en congresos, simposios y/o jornadas</MenuItem>
               <MenuItem value={'activitythree'}>Publicaciones</MenuItem>
@@ -183,48 +86,23 @@ const ActivityView = ({ className, ...rest }) => {
               <MenuItem value={'activityfive'}>Estancias de investigación en otras instituciones</MenuItem>
               <MenuItem value={'activitysix'}>participación en proyectos de investigación</MenuItem>
             </Select>
-            
-
-            
           </Grid>
-
-
-          <Grid
-
-            item
-
-            xs={12}
-          >
-
-            <TextField
-              fullWidth
-              label="Descripción"
-              name="descripcion"
-              onChange={handleChange}
-              required
-              value={values.Descripcion}
-              variant="outlined"
-            />
+          <Grid item xs={12}>
+            <TextField fullWidth label="Descripción" name="descripcion" onChange={handleChange} required value={values.Descripcion}
+              variant="outlined" />
           </Grid>
-
         </DialogContent>
         <DialogActions>
-          <Button autoFocus onClick={handleClose} color="secondary">
-            Cancelar
-          </Button>
-          <RouterLink to={activity}>
-            <Button variant="contained" color="primary">
-              Datos de detalle
-            </Button>
+          <Button autoFocus onClick={handleClose} color="secondary"> Cancelar </Button>
+          <RouterLink to={activity}> 
+            <Button variant="contained" color="primary"> Datos de detalle </Button>
           </RouterLink>
         </DialogActions>
       </Dialog>
     </div>
   );
 };
-
 ActivityView.propTypes = {
   className: PropTypes.string
 };
-
 export default ActivityView;
