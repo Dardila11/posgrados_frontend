17c2a8844c55dcc3be40786e088b2f53db824db5
faberps <44304279+faberps@users.noreply.github.com>
2020-12-16 13:56:21 -0500

    nueva ruta

--- src/routes.js
+++ src/routes.js
@@ -63,7 +63,7 @@ import DirectorListEvaluationsView from 'src/views/teamc/director/Evaluations/Li
 /* End Director imports */
 //Imports teamA
 
-import RegisterGrantView from "src/views/teamA/stundent/RegisterGrantView"
+import RegisterGrantView from "src/views/teamA/student/RegisterGrantView"
 import RegisterAgreementView from "src/views/teamA/student/RegisterAgreementView"
 import AdministerStudentView from 'src/views/teamA/coordinator/index';
 import AdministerProfileView from 'src/views/teamA/student/index';
