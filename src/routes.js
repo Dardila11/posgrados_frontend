dce72500496ff71eb3ded753212c91d621155cd6
Andres Meneses <acmeneses@unicauca.edu.co>
2020-12-16 02:53:21 -0500

    a

--- src/routes.js
+++ src/routes.js
@@ -122,6 +122,7 @@ const routes = [
     path: 'student',
     element: <StudentDashboardLayout />,
     children: [
+      { path: 'account', element: <AccountView /> },
       { path: 'administer-profile', element: <AdministerProfileView /> },
       { path: 'list-activities/activityone', element: <ActivityOneView /> },
       { path: 'list-activities/activitytwo', element: <ActivityTwoView /> },
